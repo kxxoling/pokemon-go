a5109c6dfdbd34e4bf503d83575391ac43adde01
kxxoling <kxxoling@gmail.com>
2016-07-19 00:25:47 +0800

    Implement Team Standings page

---
+++ src/components/TeamCard.vue
@@ -0,0 +1,88 @@
+<template lang="jade">
+.team-card(v-bind:class="name")
+  h2.team-name TEAM {{upperName}}
+  .team-image-container
+    img.team-image(:src="image")
+  .team-gyms
+    .controlling CONTROLLING
+    .gyms {{ gymCount }} / 300,000 GYMS
+  .team-trainers
+    .trainer-count(v-bind:class="name") {{trainerCount}}
+    .trainers {{upperName}} TRAINERS
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    gymCount: {
+      type: Number,
+      required: true,
+    },
+    trainerCount: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    upperName() {
+      return this.name.toUpperCase();
+    },
+  },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style lang="stylus">
+@import '../styles/variables'
+@import '../styles/palette'
+@import '../styles/mixins'
+
+margin-bottom()
+  line-height 100%
+  margin-bottom 20px
+
+.team-card
+  width 240px
+  background-color white
+  border-radius 4px
+  text-align center
+  color $text-grey
+
+  .team-image-container
+    background-repeat
+    background-position center
+
+  .team-name
+    padding-top 20px
+
+  .team-image
+    width auto
+    height 100px
+
+  .team-name, .team-image-container, .team-gyms, .team-trainers
+    margin-bottom()
+
+  .team-trainers
+    padding-bottom 20px
+
+  &.instinct
+    .team-name, .trainer-count
+      color $color-instinct
+  &.mystic
+    .team-name, .trainer-count
+      color $color-mystic
+  &.valor
+    .team-name, .trainer-count
+      color $color-valor
+</style>
---
+++ src/views/TeamStandings.vue
@@ -0,0 +1,62 @@
+<template lang="jade">
+.team-standing
+  h2.standing-summary 27,333,444 TRAINERS
+
+  team-card.team-card(v-for="team in teams",
+      :name="team.name",
+      :gym-count="team.gymCount",
+      :trainer-count="team.trainerCount",
+      :image="team.image")
+</template>
+
+<script>
+import TeamCard from '../components/TeamCard';
+
+const teams = [
+  {
+    name: 'mystic',
+    gymCount: 8000,
+    trainerCount: 100000,
+    image: './static/team-mystic.png',
+  },
+  {
+    name: 'instinct',
+    gymCount: 8000,
+    trainerCount: 100000,
+    image: './static/team-instinct.png',
+  },
+  {
+    name: 'valor',
+    gymCount: 8000,
+    trainerCount: 100000,
+    image: './static/team-valor.png',
+  },
+];
+export default {
+  data() {
+    return {
+      teams,
+    };
+  },
+  components: {
+    TeamCard,
+  },
+};
+</script>
+
+<style lang="stylus">
+@import '../styles/variables'
+
+.team-standing
+  display block
+
+  .standing-summary
+    color white
+    padding 30px 0
+    font-size 20px
+
+  .team-card
+    margin-bottom 30px
+    display inline-block
+
+</style>
