b7029914067a9a11597c447a994a854cce6858d5
kxxoling <kxxoling@gmail.com>
2016-07-27 18:01:32 +0800

    Add avatar to Trainer Profile page and a little style improvement

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,11 +2,11 @@
 .profile
   h2.name {{name}}
   .level LEVEL {{level}}
-  .xp-detail
-    progress-bar.xp-bar
+  .xp-details
+    progress-bar.xp-bar(:percent="34")
     .xp-data {{xp}} / {{fullXp}} XP
   .journal JOURNAL
-  .start-date Start date: {{startDateStr}}
+  .start-date START DATE: {{startDateStr}}
 </template>
 
 <script>
@@ -44,27 +44,31 @@ $line-height = 32
 margin-bottom()
   margin-bottom 16px
 
-
 .profile
   background-color white
-  height 180px
   width 200px
   padding 30px 40px 40px
   border-radius 4px
   color $text-grey
   text-align center
 
-  .name, .level, .xp-bar, .xp-data, .journal, .start-date
+  .level, .xp-bar, .start-date
     margin-bottom 16px
 
   .name
     color $blue
+    margin-bottom 40px
+    font-size 32px
 
   .level
     font-size 14px
 
+  .xp-details
+    margin-bottom 32px
+
   .journal
     width 160px
+    margin-bottom 20px
     line-height $line-height px
     border-radius ($line-height / 2) px
     color white
--- src/views/TrainerProfile.vue
+++ src/views/TrainerProfile.vue
@@ -1,12 +1,20 @@
 <template lang="jade">
-.trainer-profile
-  profile
+.trainer-profile.clearfix
+  .trainer-avatar.pull-left
+    img(src="/static/trainer-male.png", v-if="gender==='male'")
+    img(src="/static/trainer-female.png", v-if="gender==='female'")
+  profile.profile.pull-left
 </template>
 
 <script>
 import Profile from '../components/Profile';
 
 export default {
+  data() {
+    return {
+      gender: 'male',
+    };
+  },
   components: {
     Profile,
   },
@@ -14,4 +22,23 @@ export default {
 </script>
 
 <style lang="stylus">
+$padding-top = 40px
+
+.trainer-profile
+  height 100%
+
+  .trainer-avatar
+    height 100%
+    width auto
+    max-width 320px
+    margin-right 40px
+
+    img
+      padding-top $padding-top
+      max-width 100%
+      max-height 100%
+
+  .profile
+    margin-top $padding-top
+    width 320px
 </style>
